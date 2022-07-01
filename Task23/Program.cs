339344c38e06242405645eb811d0d743b17ea43a
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-01 22:12:58 +0300

    Task23 done

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, 
+//которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+void FindAQube (double n)
+{
+    double count = 0;
+    while (count <= n)
+    {
+        double qube = Math.Pow(count ,3);
+        Console.Write(qube + ", ");
+        count++;
+    }
+    return; 
+}
+Console.WriteLine("Введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+FindAQube(number);
